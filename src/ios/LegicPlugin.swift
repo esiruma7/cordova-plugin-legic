da196892baff8baddc076e0fdaf97e5472713a8b
Edwin Resuello Siruma <edwinsiruma@fareast.com>
2025-09-16 15:51:31 +0800

    Updated the cordova-plugin-legic

--- src/ios/LegicPlugin.swift
+++ src/ios/LegicPlugin.swift
@@ -5,7 +5,8 @@ import Cordova
     @objc(echo:)
     func echo(command: CDVInvokedUrlCommand) {
         let message = command.arguments.first as? String ?? "No message"
-        let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Echo: \(message)")
-        self.commandDelegate.send(result, callbackId: command.callbackId)
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK,
+                                           messageAs: "Echo: \(message)")
+        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
     }
 }
